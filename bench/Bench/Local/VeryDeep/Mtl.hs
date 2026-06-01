cb1628bd87c80eaa6be0453f441407c9d36a3670
Zhixuan Yang <s.yang20@imperial.ac.uk>
2026-06-01 01:18:53 +0100

    Downsized the very deep tests to 50+50.

--- bench/Bench/Local/VeryDeep/Mtl.hs
+++ bench/Bench/Local/VeryDeep/Mtl.hs
@@ -1,10 +1,33 @@
 #ifdef DISABLE_VERY_DEEP
 module Bench.Local.VeryDeep.Mtl where
 #else
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Bench.Local.VeryDeep.Mtl (localBench, localDeep) where
 
 import Control.Monad.Identity qualified as M
 import Control.Monad.Reader qualified as M
+
+newtype R m a = R (M.ReaderT () m a)
+
+runR :: R m a -> m a
+runR (R m) = M.runReaderT m ()
+
+instance Functor m => Functor (R m) where
+    fmap f (R m) = R (fmap f m)
+
+instance Applicative m => Applicative (R m) where
+    pure = R . pure
+    R f <*> R x = R (f <*> x)
+
+instance Monad m => Monad (R m) where
+    R m >>= f = R (m >>= \x -> case f x of R y -> y)
+
+instance M.MonadReader r m => M.MonadReader r (R m) where
+    ask = R (M.lift M.ask)
+    local f (R m) = R (M.mapReaderT (M.local f) m)
 
 program :: M.MonadReader Int m => Int -> m Int
 program = \case
@@ -15,7 +38,5 @@ localBench :: Int -> Int
 localBench n = M.runReader (program n) 0
 
 localDeep :: Int -> Int
-localDeep n = M.runIdentity $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ M.runReaderT (runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ runI $ program n) 0
-  where
-    runI = M.runIdentityT
+localDeep n = M.runIdentity $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ M.runReaderT (runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ runR $ program n) 0
 #endif
