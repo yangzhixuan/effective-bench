727d7352c57cab8db2566a9295afe2f24aa34294
Zhixuan Yang <s.yang20@imperial.ac.uk>
2026-07-06 19:07:49 +0100

    Rename the zip file

--- zipping.sh
+++ zipping.sh
@@ -1,3 +1,3 @@
-rm -f benchmark,zip
-rm -fr benchmark
-zip -r benchmark.zip . -x 'dist-newstyle/*' '.git/*' .gitignore .DS_Store
+rm -f effective-bench,zip
+rm -fr effective-bench
+zip -r effective-bench.zip . -x 'dist-newstyle/*' '.git/*' .gitignore .DS_Store
